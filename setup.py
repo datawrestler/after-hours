abc21cd0af0459269227a80a25bfc67daa56ce9d
Jason Lewris <datawrestler@gmail.com>
2015-08-26 13:42:01 -0400

    updated readme

--- setup.py
+++ setup.py
@@ -50,16 +50,21 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
         'Intended Audience :: Financial and Insurance Industry',
-        'License :: The MIT License',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Office/Business :: Financial :: Investment',
-        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Investment'
     ]
 )
